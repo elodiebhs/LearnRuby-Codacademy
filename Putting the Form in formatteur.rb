a32622e9e6266cc3106b5ba4d9de1b389004d799
elodiebhs <elodiebouthors@hotmail.com>
2021-09-05 15:57:06 -0700

    Formatting with String Methods

--- Putting the Form in formatteur.rb
+++ Putting the Form in formatteur.rb
@@ -1,14 +1,18 @@
 print "What's your first name?"
 first_name = gets.chomp
+first_name.capitalize!  # ! This modifies the value contained within the variable itself.
 
 print "What's your last name?"
 last_name = gets.chomp
+last_name.capitalize!
 
 print "What's your city?"
 city = gets.chomp
+city.capitalize!
 
 print "What's your state?"
 state = gets.chomp
+state.upcase!
 
 #string interpolation
 puts "your name is #{first_name}"

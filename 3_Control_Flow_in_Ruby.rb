6bab17a5ac267b86e7a6906cd15db698838f74bd
elodiebhs <elodiebouthors@hotmail.com>
2021-09-05 17:29:38 -0700

    true or false

--- 3_Control_Flow_in_Ruby.rb
+++ 3_Control_Flow_in_Ruby.rb
@@ -31,3 +31,16 @@ end
 #Equal or Not
 is_true = 2 != 3
 is_false = 2 == 3
+
+#True, false
+# test_1 = 77 != 77
+test_1 = false
+
+# test_2 = -4 <= -4
+test_2 = true
+
+# test_3 = -44 < -33
+test_3 = true
+
+# test_4 = 100 == 1000
+test_4 = false

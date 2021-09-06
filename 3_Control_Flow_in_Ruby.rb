f149e5b449b462918aa6aa0969c6f11e8c743d8e
elodiebhs <elodiebouthors@hotmail.com>
2021-09-05 17:20:43 -0700

    elsif statement

--- 3_Control_Flow_in_Ruby.rb
+++ 3_Control_Flow_in_Ruby.rb
@@ -10,3 +10,12 @@ if 1 > 2
 else
   print "No"
 end
+
+#Elsif
+if 1 > 2
+  print "I won't get printed because one is less than two."
+elsif 1 < 2
+  print "That means I'll get printed!"
+else
+  print "don't know then"
+end

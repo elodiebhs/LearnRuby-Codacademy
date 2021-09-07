6a3a53481c3a4b59e2dd89cce7324676c4a38dc6
elodiebhs <elodiebouthors@hotmail.com>
2021-09-07 12:18:10 -0700

    initial commit

---
+++ 8_Create_an_historygram.rb
@@ -0,0 +1,3 @@
+#In this project, we’ll write a program that takes a user’s input, then builds a hash from that input. 
+#Each key in the hash will be a word from the user; each value will be the number of times that word occurs.
+

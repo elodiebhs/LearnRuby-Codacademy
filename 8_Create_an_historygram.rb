8bd09cacda8cb6b4fcefbed634656e2b3a616008
elodiebhs <elodiebouthors@hotmail.com>
2021-09-07 12:28:02 -0700

    getting input from the user

--- 8_Create_an_historygram.rb
+++ 8_Create_an_historygram.rb
@@ -1,3 +1,5 @@
 #In this project, we’ll write a program that takes a user’s input, then builds a hash from that input. 
 #Each key in the hash will be a word from the user; each value will be the number of times that word occurs.
 
+puts "here is your statement:"
+text =  gets.chomp

70554460a3c379b272a6625565b8738c2313883d
elodiebhs <elodiebouthors@hotmail.com>
2021-09-07 16:58:11 -0700

    setup

---
+++ 12_Night_at_the_movie_project.rb
@@ -0,0 +1,12 @@
+movies = {
+  StarWars: 4.8, 
+  Divergent: 4.7
+  }
+
+puts "What would you like to do? "
+puts "-- Type 'add' to add a movie."
+puts "-- Type 'update' to update a movie."
+puts "-- Type 'display' to display all movies."
+puts "-- Type 'delete' to delete a movie."
+
+choice = gets.chomp

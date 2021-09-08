fc75becbcaa28f7d9c1f61489b7736d79695fc57
elodiebhs <elodiebouthors@hotmail.com>
2021-09-07 17:55:46 -0700

    Refactor the code in the editor to use single-line ifs and unlesss.

--- 14_Refactor_project.rb
+++ 14_Refactor_project.rb
@@ -1,15 +1,8 @@
 require 'prime' 
 
 def first_n_primes(n)
-
-  unless n.is_a? Integer
-    return "n must be an integer."
-  end
-
-  if n <= 0
-    return "n must be greater than 0."
-  end
-  
+  return "n must be an integer." unless n.is_a? Integer
+  return "n must be greater than 0." if n <= 0
   return Prime.first n
 end
 

62aef9a4e87df15a5d1e54398a3a91401bfae806
elodiebhs <elodiebouthors@hotmail.com>
2021-09-08 10:48:53 -0700

    collect mathod

--- 15_Blocks,Procs.rb
+++ 15_Blocks,Procs.rb
@@ -1,2 +1,14 @@
 #times
 5.times{puts"I'm a block!"}
+
+#collect method
+# takes a block and applies the expression in the block to every element in an array
+# return a copy, it won't mutate the original
+
+fibs = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+doubled_fibs = fibs.collect{|num|num*2}
+
+puts doubled_fibs
+
+#[2, 2, 4, 6, 10, 16, 26, 42, 68, 110]

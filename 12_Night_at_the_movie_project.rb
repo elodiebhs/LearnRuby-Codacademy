8bd75afc70061fe7b8d9cd0ca93211504090c30e
elodiebhs <elodiebouthors@hotmail.com>
2021-09-07 17:00:13 -0700

    add case statement

--- 12_Night_at_the_movie_project.rb
+++ 12_Night_at_the_movie_project.rb
@@ -10,3 +10,16 @@ puts "-- Type 'display' to display all movies."
 puts "-- Type 'delete' to delete a movie."
 
 choice = gets.chomp
+
+case choice
+when "add"
+  puts "Added!"
+when "update"
+  puts "Updated!"
+when "display"
+  puts "Movies!"
+when "delete"
+  puts "Deleted!"
+else
+  puts "Error!"
+end

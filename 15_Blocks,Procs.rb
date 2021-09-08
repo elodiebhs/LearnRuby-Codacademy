54fc861d078aa3085c1dccfde0f9ad611f3f597f
elodiebhs <elodiebouthors@hotmail.com>
2021-09-08 10:54:46 -0700

    using yield

--- 15_Blocks,Procs.rb
+++ 15_Blocks,Procs.rb
@@ -12,3 +12,18 @@ doubled_fibs = fibs.collect{|num|num*2}
 puts doubled_fibs
 
 #[2, 2, 4, 6, 10, 16, 26, 42, 68, 110]
+
+#Yield
+def block_test
+  puts "We're in the method!"
+  puts "Yielding to the block..."
+  yield
+  puts "We're back in the method!"
+end
+
+block_test { puts ">>> We're in the block!" }
+
+#We're in the method!
+#Yielding to the block...
+#>>> We're in the block!
+#We're back in the method!

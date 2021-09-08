c85cc9b9367c026c7794cc6d73137ce202af0d56
elodiebhs <elodiebouthors@hotmail.com>
2021-09-07 17:07:08 -0700

    Add an if/else statement

--- 12_Night_at_the_movie_project.rb
+++ 12_Night_at_the_movie_project.rb
@@ -15,10 +15,14 @@ choice = gets.chomp
 case choice
 when "add"
   puts "What movie would you like to add? "
-  title = gets.chomp.to_sym
-  puts "What rating does the movie have? "
-  rating = gets.chomp.to_i
-  movies[title] = rating
+  title = gets.chomp
+  if movies[title.to_sym].nil? 
+    puts "What rating does the movie have? "
+    rating = gets.chomp
+    movies[title.to_sym] = rating.to_i
+  else
+    puts "That movie already exists! Its rating is #{movies[title.to_sym]}."
+  end
 when "update"
   puts "Updated!"
 when "display"

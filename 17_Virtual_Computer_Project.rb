3c324b0677170e7dec2de6c73c4445ff6d6e7a1d
elodiebhs <elodiebouthors@hotmail.com>
2021-09-09 14:17:17 -0700

    add method create

--- 17_Virtual_Computer_Project.rb
+++ 17_Virtual_Computer_Project.rb
@@ -1,10 +1,15 @@
-lass Computer
-@@users=Hash.new
-puts @@users
-  def initialize (username, password)
-  @username=username
-  @password=password
-  @files=Hash.new
-  @@users[username] = password
+class Computer
+  @@users=Hash.new
+  puts @@users
+  
+    def initialize (username, password)
+    @username=username
+    @password=password
+    @files=Hash.new
+    @@users[username] = password
+    end
+  
+    def create(filename)
+    time=Time.now
+    end
   end
-end

e53d8671439b1701edaf7502495910d3fee45d8e
elodiebhs <elodiebouthors@hotmail.com>
2021-09-09 15:34:10 -0700

    add two private methods

--- 18_Banking_project.rb
+++ 18_Banking_project.rb
@@ -5,4 +5,13 @@ class Account
     @name = name
     @balance = balance
   end
+
+  private
+  def pin
+  @pin=1234
+  end
+
+  private def pin_error
+  return "Access denied: incorrect PIN."
+  end
 end

6b7c858c555822cc74ce931df54ad0183be235e1
elodiebhs <elodiebouthors@hotmail.com>
2021-09-17 06:57:24 +0000

    working with strings

---
+++ 20_working_with_string.rb
@@ -0,0 +1,23 @@
+#method prepend the string "Learning " to the string "Ruby"
+string = " Ruby"
+string.prepend("Learning") 
+# Learning Ruby
+
+#method concat
+string = "Good"
+string.concat(" morning")
+#=> "Good morning"
+
+#method to delete character of a string
+string = "Learning Ruby"
+string.delete "eau"
+# => "Lrning Rby" 
+
+#convert a string to number
+string ="1.23"
+string.to_f
+#  => 1.23 
+
+string ="52"
+string.to_i
+#  => 52 

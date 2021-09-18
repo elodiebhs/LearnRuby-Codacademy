a409eb6ea614fffc80d2896aed6ce8ba4a16d9c6
elodiebhs <elodiebouthors@hotmail.com>
2021-09-18 01:33:58 +0000

    working with array

--- 21_working_with_array.rb
+++ 21_working_with_array.rb
@@ -21,3 +21,31 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 numbers = numbers.select { |number| number.even? }.reverse
 numbers.delete(6)
 p numbers
+
+#Map and Capitalze
+def skip
+words = ["one", "two", "three", "four", "five"]
+words = words.select.with_index { |word, ix| ix.even? }
+words = words.map {|word| word.capitalize()}
+end
+p skip
+
+#Convert an array to a string
+def string(array)
+  "'#{array.join("', '")}'"
+end
+string(["one", "two", "three", "four", "five"])
+
+#=> "one', 'two', 'three', 'four', 'five" 
+
+#string to array
+"a b c d".split(" ")
+
+#string interpolization
+def interpo
+words = ["one", "two", "three", "four", "five"]
+words = words.select.with_index { |word, ix| ix.even? }
+words = words.map { |word| word.capitalize }
+words = words.map { |word| "#{word} <3" }
+end
+interpo
